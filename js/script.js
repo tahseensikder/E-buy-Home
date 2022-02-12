417299e7826758ee6bcd863c4b75a0d5cae77f0f
tahseensikder <tahseensikder@gmail.com>
2022-02-12 10:51:57 +0600

    backtoTop&preloader

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//preloader
+$(window).on('load',function(){
+    $('.preloader').fadeOut(1000)
+})
+$( function(){
+  $(window).on('scroll',function(){
+       if($(this).scrollTop() > 300){
+        $('#backToTop').fadeIn(1000);
+       }
+       else {
+        $('#backToTop').fadeOut(1000);
+       }
+   })
+   $('#backToTop').on('click',function(){
+       $('html,body').animate({
+           scrollTop:0,
+       },1000)
+   })
+   $('a').on('click',function(){
+       if (this.hash !== ''){
+           var hash = this.hash
+           $('html,body').animate({
+            scrollTop:$(hash).offset().top,
+        },1000)
+       }
+   })
+
+})
